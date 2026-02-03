3f34c0b5ad2067436b55cf319ea1602f2784f05a
Ashot Svazyan <ashot.svazyan222@gmail.com>
2026-02-03 22:34:03 +0300

    feat(ui): finish mobile

---
+++ layouts/home.php
@@ -0,0 +1,446 @@
+<!DOCTYPE html>
+<!--[if lt IE 6]>
+<html class="ie ie6" lang="ru" dir="ltr"> <![endif]-->
+<!--[if IE 7]>
+<html class="ie ie7" lang="ru" dir="ltr"> <![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="ru" dir="ltr"> <![endif]-->
+<!--[if IE 9]>
+<html class="ie ie9" lang="ru" dir="ltr"> <![endif]-->
+<!--[if IE 10]>
+<html class="ie ie10" lang="ru" dir="ltr"> <![endif]-->
+<!--[if (gte IE 11)|!(IE)]><!-->
+<html lang="ru" dir="ltr" xmlns="http://www.w3.org/1999/html">
+  <!--<![endif]-->
+
+  <head itemscope itemtype="http://schema.org/WPHeader">
+    <!-- DEVELOPER -->
+    <!-- https://github.com/TheWitcher1991 -->
+
+    <!-- SEO -->
+    <meta charset="UTF-8" />
+
+    <title itemprop="headline">Натуральное сливочное масло с юга России</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, viewport-fit=cover"
+    />
+    <meta itemprop="description" name="description" content="Натуральное сливочное масло с улучшенным жирнокислотным составом. Южное тепло, польза и вкус." />
+    <meta itemprop="keywords" name="keywords" content="сливочное масло, натуральное масло, южное масло, ЮМП" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta http-equiv="content-language" content="ru" />
+    <meta name="robots" content="index, follow" />
+    <link rel="canonical" href="https://poleznoemaslo.ru.ru" />
+
+    <meta name="author" content="© 2026 ООО «ЮМП»" />
+    <meta name="copyright" content="© 2026 ООО «ЮМП»" />
+
+    <meta property="og:title" content="ЮМП — настоящее сливочное масло" />
+    <meta
+      property="og:description"
+      content="Натуральное сливочное масло с улучшенным жирнокислотным составом. Южное тепло, польза и вкус."
+    />
+    <meta property="og:image:alt" content="Сливочное масло ЮМП" />
+    <meta property="og:site_name" content="ЮМП" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="ЮМП — настоящее южное масло" />
+    <meta
+      name="twitter:description"
+      content="Натуральное сливочное масло без растительных добавок"
+    />
+    <meta name="twitter:domain" content="poleznoemaslo.ru" />
+    <meta name="twitter:site" content="@poleznoemaslo.ru" />
+    <meta name="twitter:creator" content="@poleznoemaslo.ru" />
+
+    <meta name="hostname" content="poleznoemaslo.ru" />
+    <meta name="expected-hostname" content="poleznoemaslo.ru" />
+    <meta name="format-detection" content="telephone=no" />
+
+    <link rel="shortcut icon" href="../public/logo.png" type="image/x-icon" />
+    <link rel="icon" href="../public/logo.png" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="../public/logo.png" />
+
+    <script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "ЮМП",
+        "url": "https://poleznoemaslo.ru",
+        "description": "Натуральное сливочное масло с юга России"
+      }
+    </script>
+
+    <!-- LIBS -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap"
+      rel="stylesheet"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+      integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <!-- STYLES -->
+    <link
+      rel="stylesheet"
+      href="../static/font/font.css?v=<?= date('YmdHis') ?>"
+    />
+    <link
+      rel="stylesheet"
+      href="../static/styles/vars.css?v=<?= date('YmdHis') ?>"
+    />
+    <link
+      rel="stylesheet"
+      href="../static/styles/normalize.css?v=<?= date('YmdHis') ?>"
+    />
+    <link
+      rel="stylesheet"
+      href="../static/styles/wrapper.css?v=<?= date('YmdHis') ?>"
+    />
+    <link
+      rel="stylesheet"
+      href="../static/styles/media.css?v=<?= date('YmdHis') ?>"
+    />
+  </head>
+
+  <body>
+    <section class="mobile-section">
+      <nav class="flex-between mobile-nav">
+        <div class="mobile-logo">
+          <img src="../public/Logo.svg" alt="ЮМП" width="40" height="40" />
+          ЮМП
+        </div>
+        <div class="mobile-button">
+          <i class="ti mobile-action ti-menu-2"></i>
+        </div>
+      </nav>
+      <menu class="mobile-menu" itemscope itemtype="http://schema.org/ItemList">
+        <li
+          itemprop="itemListElement"
+          itemscope=""
+          itemtype="http://schema.org/ItemList"
+        >
+          <a itemprop="url" href="#mission">О нас</a>
+          <meta itemprop="name" content="О нас" />
+        </li>
+        <li
+          itemprop="itemListElement"
+          itemscope=""
+          itemtype="http://schema.org/ItemList"
+        >
+          <a itemprop="url" href="#favorite">Преимущества</a>
+          <meta itemprop="name" content="Преимущества" />
+        </li>
+        <li
+          itemprop="itemListElement"
+          itemscope=""
+          itemtype="http://schema.org/ItemList"
+        >
+          <a itemprop="url" href="#product">Продукция</a>
+          <meta itemprop="name" content="Продукция" />
+        </li>
+        <li
+          itemprop="itemListElement"
+          itemscope=""
+          itemtype="http://schema.org/ItemList"
+        >
+          <a itemprop="url" href="#footer">Контакты</a>
+          <meta itemprop="name" content="Контакты" />
+        </li>
+      </menu>
+    </section>
+
+    <div class="gs">
+      <div class="content">
+        <main id="main">
+          <header id="header">
+            <section class="section-container section-header">
+              <nav class="nav">
+                <div class="logo nav-blur wow fadeIn">
+                  <img
+                    src="../public/Logo.svg"
+                    alt="ЮМП"
+                    width="32"
+                    height="32"
+                  />
+                  ЮМП
+                </div>
+                <div class="nav-menu nav-blur wow fadeIn">
+                  <a href="#about">О команде</a>
+                  <a href="#product">Наш продукт</a>
+                  <a href="#why">Почему мы</a>
+                  <a href="#clients">Для кого</a>
+                </div>
+                <a class="nav-contact nav-blur wow fadeIn" href="#contact">Контакты</a>
+              </nav>
+              <div class="hero-block">
+                <div class="hero-text wow fadeInDown">ЮМП</div>
+                <div class="hero-caption wow fadeInUp">настоящий южный вкус</div>
+              </div>
+            </section>
+          </header>
+
+          <section class="section-about">
+            <div id="about" class="section-container">
+              <div class="section-title wow fadeInUp">
+                О нашей
+                <div class="section-title-highlight-base wow fadeInUp">команде</div>
+              </div>
+              <div class="section-about-base">
+                <div class="section-about-text wow fadeInLeft">
+                  Мы — небольшая, но очень тёплая команда из Ставрополья.
+                  <br /><span class="section-about-text-caption">
+                    Искренне верим, что натуральные продукты могут быть
+                    одновременно вкусными и по‑настоящему полезными. Именно
+                    поэтому мы делаем наше сливочное масло — 100% натуральное,
+                    без единой растительной добавки, с улучшенным жирнокислотным
+                    составом.
+                  </span>
+                </div>
+                <div class="section-about-text-second-block wow fadeInRight" data-wow-delay="0.2s">
+                  <div class="section-about-text-second">
+                    Сотрудничаем с лабораторией Ставропольского ГАУ:
+                    <span class="section-about-text-caption"
+                      >именно там определяем качество молока</span
+                    >
+                  </div>
+                  <a
+                    href="https://stgau.ru/"
+                    target="_blank"
+                    class="section-about-text-second-stgau"
+                  >
+                    <img
+                      src="../public/stgau.svg"
+                      alt="stgau"
+                      width="30"
+                      height="29"
+                    />
+                    Подробнее
+                    <i class="ti ti-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <img
+              class="section-title-image wow fadeIn"
+              src="../public/peoples.png"
+              alt="peoples"
+            />
+          </section>
+
+          <section id="product" class="section-container section-product">
+            <div class="section-title wow fadeInUp">
+              Но как
+              <div class="section-title-highlight-base">из обычного молока</div>
+              получается наше
+              <div class="section-title-highlight-second">особенное масло</div>
+            </div>
+            <div class="section-product-mix">
+              <img
+                class="section-product-image fadeInUp"
+                src="../public/mix-text.png"
+                alt="mix"
+              />
+
+              <div class="section-product-card wow fadeInUp" data-wow-delay="0s">
+                <div class="section-product-card-title">Молоко коров</div>
+                <div class="section-product-card-caption">
+                  Обладает повышенным содержанием ПНЖК (полиненасыщенные жирные
+                  кислоты)
+                </div>
+              </div>
+
+              <div class="section-product-card wow fadeInUp" data-wow-delay="0s">
+                <div class="section-product-card-title">
+                  Бережная переработка
+                </div>
+                <div class="section-product-card-caption">
+                  Сепарация и пастеризация
+                </div>
+              </div>
+
+              <div class="section-product-card wow fadeInUp" data-wow-delay="0s">
+                <div class="section-product-card-title">Современный анализ</div>
+                <div class="section-product-card-caption">
+                  ИК- спектрометрия + газовая хроматография
+                </div>
+              </div>
+
+              <div class="section-product-card wow fadeInUp" data-wow-delay="0s">
+                <div class="section-product-card-title">Бережное взбивание</div>
+                <div class="section-product-card-caption">
+                  До кремовой текстуры и золотистого масла
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section class="section-why">
+            <div id="why" class="section-container">
+              <div class="section-title wow fadeInUp">
+                почему стоит выбирать
+                <div class="section-title-highlight-white">наше масло</div>
+              </div>
+
+              <div class="section-why-text wow fadeIn">
+                Представь: тот же любимый вкус, но теперь каждый кусочек — как
+                маленький подарок для твоего здоровья!
+              </div>
+
+              <img
+                class="section-why-image wow zoomIn"
+                src="../public/maslo.png"
+                alt="maslo"
+              />
+
+              <div class="section-maslo wow fadeInLeft">
+                <img
+                  class="section-maslo-image"
+                  src="../public/maslo-1.png"
+                  alt="maslo"
+                />
+
+                <img
+                  class="section-maslo-image wow fadeInRight"
+                  src="../public/maslo-2.png"
+                  alt="maslo"
+                />
+              </div>
+            </div>
+          </section>
+
+          <section id="clients" class="section-container section-clients">
+            <div class="section-title wow fadeInUp">
+              а для чего
+              <div class="section-title-highlight-base">подойдет</div>
+              наше масло?
+            </div>
+
+            <div class="section-clients-text wow fadeIn">
+              Для всего, где нужен вкус и забота, как полезный штрих в еде —
+              главное понемножку и с любовью 🤎
+            </div>
+
+            <div class="feats">
+              <div class="feat feat-1 wow fadeInUp" data-wow-delay="0s">
+                <div class="feat-title">ДЛЯ ЗДОРОВЬЯ</div>
+                <div class="feat-caption">забота каждый день</div>
+              </div>
+              <div class="feat feat-2 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="feat-title">ДЛЯ УДОВОЛЬСТВИЯ</div>
+                <div class="feat-caption">радость в каждом кусочке</div>
+              </div>
+              <div class="feat feat-3 wow fadeInUp" data-wow-delay="0.2s">
+                <div class="feat-title">ДЛЯ КРАСОТЫ</div>
+                <div class="feat-caption">да, и так можно!</div>
+              </div>
+            </div>
+          </section>
+
+          <section id="contact" class="section-container section-contact">
+            <div class="contact-block wow fadeInUp">
+              <div class="contact-block-title">ХОТИТЕ ПОПРОБОВАТЬ?</div>
+              <div class="contact-block-caption">
+                Настоящее масло, которое заботится о тебе — вкусное, полезное и
+                с южным теплом
+              </div>
+              <a
+                class="contact-block-href"
+                href="mailto:poleznoemaslo@yandex.ru"
+                >Хочу попробовать</a
+              >
+            </div>
+          </section>
+
+          <footer id="footer">
+            <section class="section-container section-footer wow fadeIn">
+              <div class="footer-blocks">
+                <div class="logo">
+                  <img
+                    src="../public/Logo.svg"
+                    alt="ЮМП"
+                    width="48"
+                    height="48"
+                  />
+                  ЮМП
+                </div>
+
+                <div class="footer-block">
+                  <div class="footer-block-title">Для клиентов</div>
+                  <div class="footer-block-list">
+                    <a href="#about">О команде</a>
+                    <a href="#product">Наш продукт</a>
+                    <a href="#why">Почему мы</a>
+                    <a href="#clients">Для кого</a>
+                  </div>
+                </div>
+
+                <div class="footer-block">
+                  <div class="footer-block-title">Наша почта</div>
+                  <div class="footer-block-list">
+                    <a href="mailto:poleznoemaslo@yandex.ru"
+                      >poleznoemaslo@yandex.ru</a
+                    >
+                  </div>
+                </div>
+              </div>
+              <div class="footer-fasie">
+                <img src="../public/fasie.svg" width="127" height="66" />
+                Проект создан при поддержке Федерального государственного
+                бюджетного учреждения "Фонд содействия развитию малых форм
+                предприятий" в научно-технической сфере в рамках программы
+                «Студенческий стартап» федерального проекта «Платформа
+                университетского технологического предпринимательства»
+              </div>
+            </section>
+            <div>ООО «Южный молочный продукт» © 2026</div>
+          </footer>
+        </main>
+      </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"
+      integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    ></script>
+
+    <script>
+      new WOW({
+        boxClass: "wow",
+        animateClass: "animated",
+        offset: 80,
+        mobile: false,
+        live: true,
+      }).init();
+    </script>
+
+    <script src="../static/libs/gsap.min.js"></script>
+    <script src="../static/libs/ScrollTrigger.min.js"></script>
+    <script src="../static/libs/ScrollSmoother.min.js"></script>
+
+    <script src="../static/scripts/index.js?v=<?= date('YmdHis') ?>"></script>
+  </body>
+</html>
